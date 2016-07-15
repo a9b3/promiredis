5c0b179568aa1e020caac5e22529df5dfd430a98
Sam Lau <esayemm@gmail.com>
2016-07-15 01:56:36 -0700

    add some test cases

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,9 +1,43 @@
 import 'babel-polyfill'
-import should from 'should'
-import testModule from '../src/index.js'
+import redis from '../src/index.js'
+import expect from 'expect'
+import config from '../config.js'
 
-describe('tests', () => {
-  it('write tests', async () => {
-    testModule()
+describe('services/redis', () => {
+  beforeEach(() => {
+    redis.initialize(config.redis)
+  })
+
+  afterEach(async done => {
+    await redis.flushDb()
+    done()
+  })
+
+  it('redis functions should have promisified', async () => {
+    expect(redis).toExist()
+    expect(redis.getClient().getAsync).toExist()
+  })
+
+  describe('redis.keys()', () => {
+    it('return an Array', async () => {
+      const keys = await redis.keys()
+      expect(keys).toBeA(Array)
+    })
+
+    it('set a key should return Array of 1', async () => {
+      await redis.set(`foo`, 123)
+      const keys = await redis.keys()
+      expect(keys.length).toBe(1)
+      expect(keys[0]).toBe(`${config.redis.namespace}/foo`)
+    })
+  })
+
+  describe('redis.set()', () => {
+    it('123 should set key', async () => {
+      const foo = `123`
+      await redis.getClient().setAsync(`testing/foo`, foo)
+      const res = await redis.getClient().getAsync(`testing/foo`)
+      expect(res).toBe(foo)
+    })
   })
 })
